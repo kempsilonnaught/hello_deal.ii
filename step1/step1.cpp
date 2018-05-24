ff82fa92d03efbb377f5b699d120af2ee53102f5
kempsilonnaught <kemps@union.edu>
2018-05-24 15:18:43 -0400

    Makefile broken, cpp code works I think

--- step1/step1.cpp
+++ step1/step1.cpp
@@ -15,9 +15,9 @@
 using namespace dealii;
 
 void first_grid(){
-	Trangulation<2> triangulation;
+	Triangulation<2> triangulation;
 	GridGenerator::hyper_cube(triangulation); //Figure out what this line does.
-	trangulation.refine_global(4); //What does this line do?
+	triangulation.refine_global(4); //What does this line do?
 
 	std::ofstream out("grid-1.eps");
 	GridOut grid_out; //ask Jef why GridOut isn't blue
@@ -32,4 +32,30 @@ void second_grid(){
 
 	const HyperShellBoundary<2> boundary_description(center);
 	triangulation.set_boundary(0, boundary_description);
+
+	for(unsigned int step = 0; step < 5; ++step){
+		Triangulation<2>::active_cell_iterator //why no blue
+		cell = triangulation.begin_active(),
+		endc = triangulation.end();
+		for(; cell!=endc; ++cell)
+			for(unsigned int v = 0; v < GeometryInfo<2>::vertices_per_cell; ++v){
+				const double distance_from_center = center.distance(cell->vertex(v));
+
+				if(std::fabs(distance_from_center - inner_radius) < 1e-10){
+					cell->set_refine_flag();
+					break;
+				}
+			}
+		triangulation.execute_coarsening_and_refinement();	
+	}
+
+	std::ofstream out("grid-2.eps");
+	GridOut grid_out;
+	grid_out.write_eps(triangulation, out);
+	triangulation.set_boundary(0);
+}
+
+int main(){
+	first_grid();
+	second_grid();
 }
